13a846952293207f30c6e299e34e56f5a31e8257
EmirlanDev <frdevemirlan@gmail.com>
2025-03-28 07:59:10 +0600

    change cors

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -15,7 +15,14 @@ const origins = [
     "https://platform-back-qgul.onrender.com",
 ];
 const corsConfig = {
-    origin: ["http://localhost:3000"],
+    origin: (origin, callback) => {
+        if (!origin || origins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
